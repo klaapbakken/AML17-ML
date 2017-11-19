430a583880b1d6ac188bc1d00b08b061a19267f6
Øyvind Klåpbakken <oyvind.klaapbakken@gmail.com>
2017-11-19 15:38:14 +0100

    Implemented functions for counting words.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import scipy as sp
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from operator import itemgetter
 
 
 def word_split(text):
@@ -24,11 +25,29 @@ def word_split(text):
     return word_dict
 
 
-def get_all_words(start, text_list):
+def local_words(text_list):
     local_word_list = []
-    for i in range(start, len(text_list) + start):
-        text = text_list[i]
+    for i in range(len(text_list)):
+        text = text_list.iloc[i]
         word_dict = word_split(text)
         for word in word_dict:
             local_word_list.append(word)
     return local_word_list
+
+
+def local_word_dict(text_list):
+    local_word_dict = {}
+    for i in range(len(text_list)):
+        word_dict = word_split(text_list.iloc[i])
+        local_word_dict = {word: local_word_dict.get(word, 0) + word_dict.get(word, 0) for word in
+                           set(local_word_dict) | set(word_dict)}
+    return local_word_dict
+
+
+def top_words(n, word_dict):
+    assert n < len(word_dict)
+    top_dict = {}
+    sorted_keys = sorted(word_dict, key=word_dict.get, reverse=True)
+    for i in range(n):
+        top_dict[sorted_keys[i]] = word_dict[sorted_keys[i]]
+    return top_dict
